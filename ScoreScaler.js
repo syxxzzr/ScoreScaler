0e36f24c04e117a3fa3ebaf49845d81efe517b07
syxxzzr <syxxzzr@163.com>
2025-03-26 00:10:55 +0800

    Update:修复了错误的赋分算法

--- ScoreScaler.js
+++ ScoreScaler.js
@@ -1,35 +1,11 @@
-/*
-MIT License
-
-Copyright (c) 2024 syxxzzr
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-
 export const GENERAL_STANDARD = [
     [0.15, 0.35, 0.35, 0.13, 0.02],
     [
-        [100, 86],
-        [85, 71],
-        [70, 56],
-        [55, 41],
-        [40, 30]
+        [86, 100],
+        [71, 85],
+        [56, 70],
+        [41, 55],
+        [30, 40]
     ]];  // 通用赋分标准(适用于中国绝大多数采用新高考的省区)
 
 /**
@@ -50,7 +26,9 @@ export default class ScaleScore {
 
         this.standard = standard;
         this.score_all = [];
+        this.score_section = [];
 
+        this.is_reprocessed = false;
         this.is_sorted = false;
     };
 
@@ -61,38 +39,60 @@ export default class ScaleScore {
      * @private
      */
     _scale_score(raw_score) {
-        let rank = this.score_all.indexOf(raw_score);
-        if (rank === -1)
+        if (!this.score_all.includes(raw_score))
             throw new RangeError("Raw score not included in the score data.");  // 原数据不在数据集中
 
-        let percent_rank = (rank + 1) / this.score_all.length;  // 计算分数在所有分数中的百分位置
-
-        let ratio = 1;
-        for (let index = 0; index < this.standard[0].length; index++) {
-            ratio -= this.standard[0][index];  // 查找分数所处的区间
-
-            if (percent_rank > ratio) {
-                let section = this.standard[1][index];
-                return ((section[0] - section[1]) / this.standard[0][index]) * (percent_rank - ratio) + section[1];  // 进行分数映射
-            }
+        for (let i = 0; i < this.score_section.length; i++) {
+            let score_section = this.score_section[i];
+            if (raw_score > score_section[1] || raw_score < score_section[0])
+                continue;
+            return ((raw_score - score_section[0]) / (score_section[1] - score_section[0]) *
+                (this.standard[1][i][1] - this.standard[1][i][0])) + this.standard[1][i][0];  // 进行分数映射
         }
     }
 
     /**
      * 为源数据集排序
+     * @param reprocess_score_section {boolean} 是否在排序后自动重新计算不同分数段的分数区间 (默认重新计算)
      * @private
      */
-    _sort() {
-        if (!this.is_sorted) {  // 未排序则对分数进行排序
-            if (this.score_all.length <= this.standard.length) {
-                throw new RangeError("The dataset is too small to scale score.")  // 数据集过小,无法进行赋分操作
-            }
+    _sort(reprocess_score_section = true) {
+        if (this.is_sorted)
+            return;  // 已排序则不再重新排序
 
-            this.score_all.sort(function (a, b) {
-                return a - b;
-            });
+        if (this.score_all.length < 1 / this.standard[0].min) {
+            throw new RangeError("The dataset is too small to scale score.")  // 数据集过小,无法进行赋分操作
+        }
 
-            this.is_sorted = true;
+        this.score_all.sort(function (a, b) {
+            return a - b;
+        });
+        this.is_sorted = true;
+
+        // 重新计算不同分数段的分数区间
+        if (reprocess_score_section) {
+            this._reprocess_score_section();
+            this.is_reprocessed = true;
+        }
+        else
+            this.is_reprocessed = false;
+    }
+
+    /**
+     * 重新计算不同分数段的分数区间
+     * @private
+     */
+    _reprocess_score_section() {
+        if (!this.is_sorted)
+            this._sort();
+
+        this.score_section = [];
+        let ratio = 1;
+        for (const ratio_delta of this.standard[0]) {
+            let section_end = this.score_all[parseInt(this.score_all.length * ratio) - 1];
+            ratio -= ratio_delta;
+            let section_start = this.score_all[parseInt(this.score_all.length * ratio) - 1];
+            this.score_section.push([section_start, section_end]);
         }
     }
 
@@ -143,13 +143,9 @@ export default class ScaleScore {
         this._sort();  // 对原成绩排序
 
         let last_score = null;
-        for (const score of this.score_all) {
-            if (score !== last_score)  // 防止对相同分数进行多次赋分运算
+        for (const score of this.score_all)
+            if (score !== last_score)  // 防止对相同分数进行多次的赋分运算
                 table[score] = this._scale_score(score);
-
-            else
-                continue;
-        }
 
         return table;
     }
--- example.js
+++ example.js
@@ -2,14 +2,14 @@ import ScaleScore, {GENERAL_STANDARD} from "./ScoreScaler.js";
 
 let scale_score = new ScaleScore(GENERAL_STANDARD);  // 创建赋分实例
 
-scale_score.update(1);  // 添加创建数据
-scale_score.update([ 2, 3, 4, 5 ]);  // 添加创建数据
+scale_score.update(1);  // 添加原始分数数据
+scale_score.update([...Array(99).keys()].map(i => i + 2));  // 添加原始分数数据 (2-100 均匀分布的99项数组)
 
 // 执行赋分操作
-console.log(`1 在 [ 1, 2, 3, 4, 5 ] 中赋分后成绩为:\n${scale_score.scale(1)}\n`);
+console.log(`1 在 1-100 均匀分布的100项原始分数据集中赋分后成绩为:\n${scale_score.scale(1)}\n`);
 
 // 执行赋分操作
-console.log(`[ 2, 3, 4, 5 ] 在 [ 1, 2, 3, 4, 5 ] 中赋分后成绩为:\n[${scale_score.scale([2, 3, 4, 5])}]\n`);
+console.log(`[ 2, 3, 4, 5 ] 在 1-100 均匀分布的100项原始分数据集中赋分后成绩为:\n[${scale_score.scale([2, 3, 4, 5])}]\n`);
 
 // 输出所有原成绩赋分后的原成绩-赋分对照表
-console.log(`所有原成绩赋分后的原成绩-赋分对照表为:\n${JSON.stringify(scale_score.scale_all())}`);
+console.log(`1-100 均匀分布的100项原始分数集赋分后的原成绩-赋分对照表为:\n${JSON.stringify(scale_score.scale_all())}`);
